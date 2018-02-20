2ef10d690a6496b7eb5c8c14b88caa70bb22c704
Cristiano de Oliveira <cristianospsp@gmail.com>
2018-02-20 20:45:21 -0300

    teste com: RandomUtils.useTestSeed()

--- src/main/java/br/com/caelum/recomendador/Avaliador.java
+++ src/main/java/br/com/caelum/recomendador/Avaliador.java
@@ -5,6 +5,7 @@ import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.common.RandomUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,9 @@ public class Avaliador {
 
 
 	public static void main(String[] args) throws IOException, TasteException {
+
+		RandomUtils.useTestSeed();
+
 		File file = new File("dados.csv");
 		FileDataModel model = new FileDataModel(file);
 
