1c0b545c07fb6e704c97a0629e6d3e56b74d2362
Cristiano de Oliveira <cristianospsp@gmail.com>
2018-02-20 21:50:27 -0300

    refactor

--- src/main/java/br/com/caelum/recomendador/Avaliador.java
+++ src/main/java/br/com/caelum/recomendador/Avaliador.java
@@ -4,10 +4,9 @@ import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.common.RandomUtils;
 
-import java.io.File;
 import java.io.IOException;
 
 public class Avaliador {
@@ -17,13 +16,12 @@ public class Avaliador {
 
 		RandomUtils.useTestSeed();
 
-		File file = new File("dados.csv");
-		FileDataModel model = new FileDataModel(file);
+		DataModel produtos = new Recomendador().getModeloDeCursos();
 
 		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
-		RecommenderBuilder builder = new RecomendadorDeProdutosBuilder();
+		RecommenderBuilder builder = new RecomendadorBuilder();
 
-		double erro = evaluator.evaluate(builder, null, model, 0.9, 1.0);
+		double erro = evaluator.evaluate(builder, null, produtos, 0.9, 1.0);
 
 		System.out.println(erro);
 	}
--- src/main/java/br/com/caelum/recomendador/RecomendadorBuilder.java
+++ src/main/java/br/com/caelum/recomendador/RecomendadorBuilder.java
@@ -8,16 +8,14 @@ import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
-import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
-public class RecomendadorDeProdutosBuilder implements RecommenderBuilder {
+public class RecomendadorBuilder implements RecommenderBuilder {
 
 	public Recommender buildRecommender(DataModel model) throws TasteException {
 		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
 		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
-		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-		return recommender;
+		return new GenericUserBasedRecommender(model, neighborhood, similarity);
 	}
 
 }
